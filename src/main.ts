5ed3d881d77d085e2cec4a96a244aa4d0d87cdc6
olowo <olowodarey8@gmail.com>
2025-05-14 12:04:33 +0100

    feat: implement validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ // this will be the global validation and will also throw out what is not included in our list 
+    whitelist: true,
+    forbidNonWhitelisted: true, 
+    transform: true, // it will convert the incoming object to and instance of Dto
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,27 +10,37 @@ import {
   Param,
   Headers,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { request } from 'http';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-params';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/{:optional}')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
-    console.log(limit);
+  @Get('/:id')
+  public getUsers(
+    @Param() getUsersParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(getUsersParamDto);
+
     return 'You sent a get request to user endpoints ';
   }
 
   @Post()
-  public createUsers(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any,
-  ) {
-    console.log(request);
-    console.log(headers);
-    console.log(ip);
+  public createUsers(@Body() createUsersDto: CreateUserDto) {
+    console.log(createUsersDto);
+
     return 'You sent a get request to user endpoints ';
+  }
+
+  @Patch()
+  public patchUser(@Body() PatchUserDto: PatchUserDto) {
+    return PatchUserDto;
   }
 }
